09ee8a6e881cc0b2ab958891f874882d7e8d97ed
Yassine Boumajane <yassine_boumajane@carrefour.com>
2020-09-16 19:54:16 +0200

    Pushing some ansible code

---
+++ up.sh
@@ -0,0 +1,2 @@
+az login --service-principal -u 96de3350-0e63-4ba4-b858-2935c2dc12ea -p 2zbc~.fx6-El5fuzGN~jRt~KTSTG35mp9z --tenant 67007139-5721-4225-b1c1-dba1c8765f3c
+az vm start -g devops_group -n devops
